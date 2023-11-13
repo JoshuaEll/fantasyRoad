012725e9502d95286693deef584a3916a6187858
Joshua Ellis <joshuaell131@gmail.com>
2023-11-13 14:33:08 -0600

    First commit

---
+++ src/main/java/com/joshell/fantasyroad/FantasyRoadApplication.java
@@ -0,0 +1,13 @@
+package com.joshell.fantasyroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyRoadApplication.class, args);
+	}
+
+}
